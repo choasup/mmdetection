2a6d958110f12253dee0bb829017bb67bea282d0
choasliu <choasliu@tencent.com>
2020-08-19 15:42:59 +0800

    private training

--- run_single.sh
+++ run_single.sh
@@ -1,7 +1,11 @@
 export TORCH_HOME=/youtu/xlab-team4/choasliu/pretrained
 
-$CONFIG=configs/research/
-$LOGS=/apdcephfs/private_choasliu/logs/mmdetection/$CONFIG
+# only need set your config
+CONFIG=configs/_algo/fcos_r50_caffe_fpn_4x4_1x_coco.py
+#CONFIG=configs/_algo/retinanet_r50_fpn_1x_coco.py
+
+ALGO=${CONFIG#*/}
+LOGS=/apdcephfs/private_choasliu/logs/mmdetection/${ALGO////-}
 
 python3 tools/train.py $CONFIG \
     --work-dir $LOGS
