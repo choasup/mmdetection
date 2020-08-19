326e879970f4ecc3c707f8d1cdb6768c964b23ca
rpf <rpf@tencent.com>
2020-08-19 15:25:35 +0800

    private set

--- run_single.sh
+++ run_single.sh
@@ -1,6 +1,6 @@
 export TORCH_HOME=/youtu/xlab-team4/choasliu/pretrained
 
-$CONFIG=configs/research/faster_rcnn_r50_fpn_1x_coco.py
+$CONFIG=configs/research/
 $LOGS=/apdcephfs/private_choasliu/logs/mmdetection/$CONFIG
 
 python3 tools/train.py $CONFIG \
