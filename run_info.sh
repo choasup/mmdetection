639e4f82c42fc8c7dabdb87fd5cc43c4c361ad8a
choasliu <choasliu@tencent.com>
2020-08-25 20:16:44 +0800

    tanners 2x

--- run_info.sh
+++ run_info.sh
@@ -1,8 +1,21 @@
-python3 tools/analyze_logs.py plot_curve /youtu-xlab4/choasliu/research/logs/_algo-retinanet_r50_fpn_1x_coco.py/20200820_114159.log.json --keys loss_cls loss_bbox --out losses.pdf
+LOG1=/youtu-xlab4/choasliu/research/logs/_algo-retinanet_r50_fpn_1x_coco.py/20200820_114159.log.json
+LOG2=/youtu-xlab4/choasliu/research/logs/_algo-fcos_r50_caffe_fpn_4x4_1x_coco.py/20200820_114118.log.json
+LOG3=/youtu-xlab4/choasliu/research/logs/_algo-tanners_r50_caffe_fpn_4x4_1x_coco.py/20200825_121301.log.json
+LOG4=/youtu-xlab4/choasliu/research/logs/_algo-tanners_r50_caffe_fpn_4x4_2x_coco.py/20200825_122452.log.json
+LOG5=/youtu-xlab4/choasliu/research/logs/_algo-tanners_r50_caffe_fpn_4x4_1x_lr_coco.py/20200825_144131.log.json
+
+#python3 tools/analyze_logs.py plot_curve \
+#    $LOG3 \
+#    --keys loss_cls loss_bbox \
+#    --out losses.pdf
+
 python3 tools/analyze_logs.py plot_curve \
-    /youtu-xlab4/choasliu/research/logs/_algo-fcos_r50_caffe_fpn_4x4_1x_coco.py/20200820_114118.log.json \
-    /youtu-xlab4/choasliu/research/logs/_algo-retinanet_r50_fpn_1x_coco.py/20200820_114159.log.json \
+    $LOG1 \
+    $LOG2 \
+    $LOG3 \
+    $LOG5 \
     --keys bbox_mAP \
-    --legend run1 run2 \
+    --legend retina fcos tanner1 tanner2 \
     --out mAP.pdf
+
 python3 tools/analyze_logs.py cal_train_time /youtu-xlab4/choasliu/research/logs/_algo-fcos_r50_caffe_fpn_4x4_1x_coco.py/20200820_114118.log.json
