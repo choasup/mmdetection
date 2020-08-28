0de65f49cf55f6574d4b23b142d7ed40a34054fb
choasliu <choasliu@tencent.com>
2020-08-28 15:39:33 +0800

    camera

--- run_dist.sh
+++ run_dist.sh
@@ -1,11 +1,11 @@
 export TORCH_HOME=/youtu/xlab-team4/share/pretrained
 
 # only need set your config
-CONFIG=configs/_algo/tanners_r50_caffe_fpn_4x4_lr0.02_b4_1x_coco.py
+CONFIG=configs/_algo/tanners-multi_r50_caffe_fpn_4x4_syncbn_lr0.02_b4_1x_coco.py
 #CONFIG=configs/_algo/retinanet_r50_fpn_1x_coco.py
 
 ALGO=${CONFIG#*/}
-LOGS=/youtu/xlab-team4/choasliu/research/logs/_algo-w1-tanners_r50_caffe_fpn_4x4_lr0.02_b4_1x_coco.py
+LOGS=/youtu/xlab-team4/choasliu/research/logs/_algo-w1-tanners-multi_r50_caffe_fpn_4x4_syncbn_lr0.02_b4_1x_coco.py
 
 #python3 tools/train.py $CONFIG \
 #    --work-dir $LOGS
